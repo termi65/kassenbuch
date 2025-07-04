8991c5bf691dbd4e77122a9b8948eab57944ad80
Markus <m.hoffmann65@gmx.de>
2025-07-04 15:29:33 +0200

    Supabase

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import supabase from '../assets/supabase';
 
 export default function Home() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const checkUser = async () => {
+            const { data } = await supabase.auth.getUser();
+            setUser(data?.user);
+        }
+        checkUser();
+
+        const {data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+            setUser(session?.user || null);
+            return () => authListener?.subscription.unsubscribe();
+        })
+
+    },[]);
     return (
         <div>
             <h1>Kassenbuch</h1>
+            <div><p className="text-decoration-underline">{user ? `angemeldet als ${user.identities[0].email}` : 'Gast'}</p></div>
         </div>
     )
 }
